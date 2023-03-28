67806c7063cac4452e1716fb265c6b5c4289f23d
Aldrich Cervania <aldrich@cervania.com>
2023-03-28 16:00:48 +0100

    Initial setup and showing some questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,20 +1,37 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Why are you gae?",
+        "Who says I'm gae?",
+        "You r gae"
+    ]
+    
+    var questionNumber = 0 //A reference to track the question the user is reading
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        progressBar.layer.cornerRadius = 8
+        progressBar.clipsToBounds = true
+        updateUI()
+        
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 }
 
