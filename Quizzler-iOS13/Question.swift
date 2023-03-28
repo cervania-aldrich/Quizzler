f57ec537efeb7a77f970b305876c479bbb7b42fd
Aldrich Cervania <aldrich@cervania.com>
2023-03-28 17:21:48 +0100

    Create a Question Struct to replace 2D array

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Question {
+    
+    let text:String
+    let answer:String
+    
+}
+
+// You do not need to initialize structs (i.e. write init() code in structs). 
+// It's all because structs are value-types, and classes are reference-types.
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -7,13 +7,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var falseButton: UIButton!
     
+    //quiz is now an array of Question objects, instead of stored as a 2D array.
+    //quiz[0] accesses Question Object. quiz[1] accesses another Question Object, etc. All instead of a nested array.
+    
     let quiz = [
-        ["1 x 5 = 5", "True"],
-        ["6 + 3 = 7", "False"],
-        ["3 + 3 = 9", "False"]
+        Question(text: "1 x 5 = 5", answer: "True"),
+        Question(text: "2 + 9 = 11", answer: "False"),
+        Question(text: "6 x 3 = 18", answer: "True"),
+        Question(text: "3 + 8 = 32", answer: "False"),
+        Question(text: "4 x 4 = 12", answer: "False"),
+        Question(text: "5 + 17 = 23", answer: "True"),
+        Question(text: "8 x 9 = 72", answer: "True"),
+        Question(text: "23 + 12 = 26", answer: "False"),
+        Question(text: "6 x 3 = 18", answer: "True"),
+        Question(text: "6 / 3 = 2", answer: "True"),
+        Question(text: "12 x 5 = 60", answer: "True"),
+        Question(text: "3 + 3 = 9", answer: "False")
     ]
     
-    var questionNumber = 0 //A reference to track the question the user is reading
+    var questionNumber = 0 //A reference to track which question the user is currently on.
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +41,7 @@ class ViewController: UIViewController {
         //Check the answer they picked against the real answer (From a 2D Array)
         
         let userAnswer = sender.currentTitle!
-        let actualAnswer = quiz[questionNumber][0]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
             print("Correct!")
@@ -50,7 +62,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI(){
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text //Access the text property of the (Question) object stored at quiz[questionNumber].
     }
     
 }
